f76dad718d3ed8c9df46d48dbd38e41116acfe7b
Yuki.N <Syukkic@gmail.com>
2024-12-08 19:01:24 +0800

    impl random listening port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,13 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -16,8 +18,14 @@ async fn health_check_works() {
 
 // Launch our application in the background
 // API endpoints
-fn spawn_app() {
-    let server = feedwave::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = feedwave::run(listener).expect("Failed to bind address");
 
     tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
