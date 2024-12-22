09289e1eb1d6d96cf69efca2139e8478ecbe647c
Yuki.N <Syukkic@gmail.com>
2024-12-22 14:38:49 +0800

    Modified config for `ENV

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use feedwave::configuration::get_configuration;
 use feedwave::starter::run;
 use feedwave::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -10,12 +11,15 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("FeedWave".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
