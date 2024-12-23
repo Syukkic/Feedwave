d47147028dd63e4fd1614451b3d07a413a924739
Yuki.N <Syukkic@gmail.com>
2024-12-23 23:29:01 +0800

    Split submodules

---
+++ src/domain/subscriber_email.rs
@@ -0,0 +1,67 @@
+use validator::ValidateEmail;
+
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<SubscriberEmail, String> {
+        if ValidateEmail::validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SubscriberEmail;
+    use claims::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::rand::rngs::StdRng;
+    use fake::rand::SeedableRng;
+    use fake::Fake;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(g.size() as u64);
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+
+            // // the trait `RngCore` is not implemented for `Gen`, which is required by `Gen: Rng`
+            // let email = SafeEmail().fake_with_rng(g);
+            // Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(vaild_email: ValidEmailFixture) -> bool {
+        dbg!(&vaild_email.0);
+        SubscriberEmail::parse(vaild_email.0).is_ok()
+    }
+}
